1ee253c8e4840ee442bd4d0c57867995e52c97a7
Steve Lambert <steve.lambert67@gmail.com>
2021-01-18 19:44:41 +0000

    wip

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 
 
-
+// passport.js
+// middleware integrating with express handling authentication logic
+// passport.js has multiple strategies
+// - passport-local
+// - passport-jwt
+// - passport-oauth2
 
 app.listen(3000);
